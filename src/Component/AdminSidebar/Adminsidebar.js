b1229112dfb5099d0252cfd27d03d693e4807b32
ArnabC49 <46952316+codeR49@users.noreply.github.com>
2022-05-18 18:24:20 +0530

    modal n sidebar color

--- src/Component/AdminSidebar/Adminsidebar.js
+++ src/Component/AdminSidebar/Adminsidebar.js
@@ -21,25 +21,27 @@ function Adminsidebar() {
             </div>
     <div className="sidebar-center">
         <ul className="list">
-           
+        <Link to='/admin/viewdata' className='Link'>
             <li className="list-item">
-              <Link to='/admin/viewdata' className='Link'>
+     
               <i className="list-item-icon fas fa-file"></i>
                 <span className="list-item-text">View Data</span>
 
-              </Link>
+              
             </li>
-           
+            </Link>
         </ul>
     </div>
     <div className="sidebar-bottom">
        <ul className='list'>
+       <Link to="/" className='Link' >
        <li className="list-item">
-           <Link to="/" className='Link' >
+           
                 <i className="list-item-icon fas fa-sign-out-alt"></i>
                 <span className="list-item-text" onClick={logoutHnadler}>logout</span>
-                </Link>
+              
             </li>
+            </Link>
        </ul>
     </div>
 </div>
